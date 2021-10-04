b4c50aa16b4447bbb3695f6f5d0fedde24400ac9
dang <trinhdangnhat@gmail.com>
2021-10-05 00:30:44 +0200

    password hasher service in test & alice bundlde resset DB

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,7 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
 ];
--- tests/Functional/CheeseListingResourceTest.php
+++ tests/Functional/CheeseListingResourceTest.php
@@ -2,17 +2,29 @@
 namespace App\Tests\Functional;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
+use App\Test\CustomApiTestCase;
+use Doctrine\ORM\EntityManagerInterface;
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
-class CheeseListingResourceTest extends ApiTestCase
+class CheeseListingResourceTest extends CustomApiTestCase
 {
+    use ReloadDatabaseTrait; // reset DATABASE each test
     public function testCreateCheeseListing(){
 
         $client = self::createClient();
         $client->request('POST', '/api/cheeses',[
             //'headers'=> ['Content-Type'=>'text/html'],
             //'json'=>[]
+
+
         ]);
         $this->assertResponseStatusCodeSame(401);
+
+        $this->createUser('tom@yahoo.com','tom');
+
+        $this->logIn($client, 'tom@yahoo.com','tom');
+
 
     }
 }
